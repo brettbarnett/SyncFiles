527699b330ce55a33dcf4e71229600020be75c7a
unknown <brett_barnett@sbcglobal.net>
2025-08-22 09:35:39 -0500

    First Commit

---
+++ BackupClient.cs
@@ -0,0 +1,228 @@
+﻿using Renci.SshNet;
+
+namespace SyncFiles;
+
+internal class BackupClient
+{
+    readonly string backupServer;
+    readonly int backupPort;
+    readonly string username;
+    readonly string password;
+    readonly string remoteWorkingDirectory;
+    readonly string localWorkingDirectory;
+    readonly string defaultHomeDirectory = "/mnt";
+    readonly SftpClient sftpClient;
+
+    string remoteUserProfilePath = "";
+
+    public BackupClient(string BackupServer, int BackupPort, string Username, string Password, string LocalWorkingDirectory, string RemoteWorkingDirectory)
+    {
+        backupServer = BackupServer;
+        backupPort = BackupPort;
+        username = Username;
+        password = Password;
+        localWorkingDirectory = LocalWorkingDirectory;
+        remoteWorkingDirectory = RemoteWorkingDirectory;
+
+        sftpClient = new SftpClient(backupServer, backupPort, username, password);
+    }
+
+    public bool Connect()
+    {
+        try
+        {
+            sftpClient.Connect();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public void CreateBackup(string PathToLocalDirectory)
+    {
+        if (string.IsNullOrWhiteSpace(remoteUserProfilePath))
+        {
+            CreateUserProfileDirectory();
+        }
+
+        CreateRemoteDirectoryStructure(PathToLocalDirectory);
+
+        string[] files = Directory.GetFiles(PathToLocalDirectory, "*", SearchOption.AllDirectories);
+        foreach (string file in files)
+        {
+            DateTime localModifiedDateTime = DateTime.Parse(File.GetLastWriteTime(file).ToString("G"));
+
+            string remoteFilePath = ConvertLocalFilePathToRemoteFilePath(file);
+
+            if (sftpClient.Exists(remoteFilePath))
+            {
+                DateTime remoteModifiedDateTime = DateTime.Parse(sftpClient.GetLastWriteTime(remoteFilePath).ToString("G"));
+
+                //Spectre.Console.AnsiConsole.MarkupLine($"[yellow bold]Local: {localModifiedDateTime}, Remote: {remoteModifiedDateTime}[/]");
+                
+                if (localModifiedDateTime <= remoteModifiedDateTime)
+                {
+                    Spectre.Console.AnsiConsole.MarkupLine($"[yellow bold]Skipping (already current): {file}[/]");
+                    continue; // Skip if the file is not modified
+                }
+            }
+
+            try
+            {
+                using (FileStream fileToUpload = File.OpenRead(file))
+                {
+                    sftpClient.UploadFile(File.OpenRead(file), remoteFilePath);
+                    sftpClient.SetLastWriteTime(remoteFilePath, localModifiedDateTime);
+                    Spectre.Console.AnsiConsole.MarkupLine($"[green bold]Uploaded local file: {file}[/]");
+                    Spectre.Console.AnsiConsole.MarkupLine($"[green bold]Uploaded remote file: {remoteFilePath}[/]");
+                }
+            }
+            catch (Exception ex)
+            {
+                Spectre.Console.AnsiConsole.MarkupLine($"[red bold]Failed: {file}[/]");
+                Spectre.Console.AnsiConsole.MarkupLine($"[red bold]Error: {ex.Message}[/]");
+            }
+
+            Spectre.Console.AnsiConsole.WriteLine();
+        }
+    }
+
+    public void CreateRemoteDirectoryStructure(string PathToLocalDirectory)
+    {
+        if (!sftpClient.Exists(ConvertLocalFolderPathToRemoteFolderPath(PathToLocalDirectory)))
+        {
+            sftpClient.CreateDirectory(ConvertLocalFolderPathToRemoteFolderPath(PathToLocalDirectory));
+            sftpClient.SetLastWriteTime(ConvertLocalFolderPathToRemoteFolderPath(PathToLocalDirectory), Directory.GetLastWriteTime(PathToLocalDirectory));
+        }
+
+        string[] directories = Directory.GetDirectories(PathToLocalDirectory, "*", SearchOption.AllDirectories);
+        foreach (string directory in directories)
+        {
+            string path = ConvertLocalFolderPathToRemoteFolderPath(directory);
+
+            // Consistent forward slashes
+            path = path.Replace(@"\", "/");
+            foreach (string dir in path.Split('/'))
+            {
+                if (string.IsNullOrWhiteSpace(dir))
+                {
+                    continue; // Ignoring leading/ending/multiple slashes
+                }
+                if (sftpClient.WorkingDirectory.Equals($"/{dir}"))
+                {
+                    continue; // Skip if the directory is the current working directory
+                }
+
+                if (!sftpClient.Exists(dir))
+                {
+                    sftpClient.CreateDirectory(dir);
+                }
+                sftpClient.ChangeDirectory(dir);
+            }
+            // Going back to default directory
+            sftpClient.ChangeDirectory(defaultHomeDirectory);
+        }
+    }
+
+    public void RestoreBackup(string PathToRemoteDirectory)
+    {
+        CreateLocalDirectoryStructure(PathToRemoteDirectory);
+
+        string localFolderPath = ConvertRemoteFolderPathToLocalFolderPath(PathToRemoteDirectory);
+        string[] files = sftpClient.ListDirectory(PathToRemoteDirectory)
+            .Where(f => !f.IsDirectory && f.Name != "." && f.Name != "..")
+            .Select(f => f.FullName)
+            .ToArray();
+
+        foreach (string file in files)
+        {
+            DateTime remoteModifiedDateTime = DateTime.Parse(sftpClient.GetLastWriteTime(file).ToString("G"));
+
+            string localFilePath = ConvertRemoteFilePathToLocalFilePath(file);
+
+            if (File.Exists(localFilePath))
+            {
+                DateTime localModifiedDateTime = DateTime.Parse(File.GetLastWriteTime(localFilePath).ToString("G"));
+                if (localModifiedDateTime >= remoteModifiedDateTime)
+                {
+                    Spectre.Console.AnsiConsole.MarkupLine($"[yellow bold]Skipping (already current): {file}[/]");
+                    continue; // Skip if the file is not modified
+                }
+            }
+            try
+            {
+                using (FileStream fileToDownload = File.Create(localFilePath))
+                {
+                    sftpClient.DownloadFile(file, fileToDownload);
+                    sftpClient.SetLastWriteTime(file, remoteModifiedDateTime);
+                    Spectre.Console.AnsiConsole.MarkupLine($"[green bold]Downloaded remote file: {file}[/]");
+                    Spectre.Console.AnsiConsole.MarkupLine($"[green bold]Saved locally: {localFilePath}[/]");
+                }
+            }
+            catch (Exception ex)
+            {
+                Spectre.Console.AnsiConsole.MarkupLine($"[red bold]Failed: {file}[/]");
+                Spectre.Console.AnsiConsole.MarkupLine($"[red bold]Error: {ex.Message}[/]");
+            }
+            Spectre.Console.AnsiConsole.WriteLine();
+        }
+    }
+
+    public void CreateLocalDirectoryStructure(string PathToRemoteDirectory)
+    {
+        string localFolderPath = ConvertRemoteFolderPathToLocalFolderPath(PathToRemoteDirectory);
+        if (!Directory.Exists(localFolderPath))
+        {
+            Directory.CreateDirectory(localFolderPath);
+        }
+        string[] directories = sftpClient.ListDirectory(PathToRemoteDirectory).Where(d => d.IsDirectory && d.Name != "." && d.Name != "..").Select(d => d.FullName).ToArray();
+        foreach (string directory in directories)
+        {
+            CreateLocalDirectoryStructure(directory);
+        }
+    }
+
+    private void CreateUserProfileDirectory()
+    {
+        if (!sftpClient.Exists(remoteWorkingDirectory))
+        {
+            sftpClient.CreateDirectory(remoteWorkingDirectory);
+        }
+        sftpClient.ChangeDirectory(remoteWorkingDirectory);
+        remoteUserProfilePath = sftpClient.WorkingDirectory;
+        sftpClient.ChangeDirectory("/mnt");
+    }
+
+    private string ConvertLocalFilePathToRemoteFilePath(string PathToLocalFile)
+    {
+        char driveLetter = localWorkingDirectory[0];
+
+        string remoteFilePath = PathToLocalFile.Trim().Replace("\\", "/");
+        return remoteFilePath.Replace($"{driveLetter}:/Users/{username}/", remoteWorkingDirectory);
+    }
+
+    private string ConvertLocalFolderPathToRemoteFolderPath(string PathToLocalFolder)
+    {
+        char driveLetter = localWorkingDirectory[0];
+
+        string remoteFolderPath = PathToLocalFolder.Trim().Replace("\\", "/");
+        return remoteFolderPath.Replace($"{driveLetter}:/Users/{username}/", remoteWorkingDirectory);
+    }
+
+    private string ConvertRemoteFilePathToLocalFilePath(string PathToRemoteFile)
+    {
+        char driveLetter = localWorkingDirectory[0];
+
+        string localFilePath = PathToRemoteFile.Trim().Replace("/", "\\");
+        return localFilePath.Replace(remoteWorkingDirectory, $"{driveLetter}:\\Users\\{username}\\");
+    }
+
+    private string ConvertRemoteFolderPathToLocalFolderPath(string PathToRemoteFolder)
+    {
+        char driveLetter = localWorkingDirectory[0];
+
+        return PathToRemoteFolder.Trim().Replace(remoteWorkingDirectory, $"{driveLetter}:\\Users\\{username}\\").Replace("/", "\\");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using SyncFiles;
+
+string version = "1.0.0";
+
+string ftpUser = "brett";
+string ftpPassword = "FVa-09'%P>6w";
+string ftpServer = "10.0.0.2";
+int ftpPort = 22;
+
+string localUser = Environment.UserName;
+string localUserDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+string remoteUserDirectory = $"/mnt/array1/Backup/{localUser}/";
+
+Spectre.Console.AnsiConsole.WriteLine("Welcome to the User Profile Backup Utility");
+Spectre.Console.AnsiConsole.MarkupLine($"Version: [bold underline green]{version}[/]");
+
+BackupClient backupClient = new BackupClient(ftpServer, ftpPort, ftpUser, ftpPassword, localUserDirectory, remoteUserDirectory);
+if (!backupClient.Connect())
+{
+    Console.WriteLine("Failed to connect to the backup server.");
+    return;
+}
+
+Console.WriteLine("1. Backup User Profile");
+Console.WriteLine("2. Restore User Profile");
+Console.WriteLine("3. Exit");
+Console.Write("Please select an option (1-3): ");
+string? choice = Console.ReadLine();
+switch (choice)
+{
+    case "1":
+        BackupUserProfile();
+        break;
+    case "2":
+        RestoreUserProfile();
+        break;
+    case "3":
+        Console.WriteLine("Exiting the utility.");
+        break;
+    default:
+        Console.WriteLine("Invalid choice. Please select 1, 2, or 3.");
+        break;
+}
+
+void BackupUserProfile()
+{
+    string[] backupFolders =
+    [
+        Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+        Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+        Environment.GetFolderPath(Environment.SpecialFolder.MyMusic),
+        Environment.GetFolderPath(Environment.SpecialFolder.MyVideos),
+        Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+        Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\source"
+    ];
+
+    foreach (string folder in backupFolders)
+    {
+        backupClient.CreateBackup(folder);
+    }
+}
+
+void RestoreUserProfile()
+{
+    backupClient.RestoreBackup(remoteUserDirectory);
+}
